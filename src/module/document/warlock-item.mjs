90b7a34aa5127d9c7cf89b80f026b015e5be9453
Barry Robertson <gmsshadow@hotmail.com>
2023-11-09 10:32:22 +0000

    Changed skill to Wetwired skills on sheets

--- src/module/document/warlock-item.mjs
+++ src/module/document/warlock-item.mjs
@@ -159,12 +159,10 @@ export class WarlockItem extends Item {
         } else if (activeSystem === "warpstar") {
             this.system.skill.choices = {
                 "—": "—",
-                "Blades": game.i18n.localize("WARLOCK.Skills.Blades"),
-                "Blunt": game.i18n.localize("WARLOCK.Skills.Blunt"),
-                "Brawling": game.i18n.localize("WARLOCK.Skills.Brawling"),
-                "Ship gunner": game.i18n.localize("WARLOCK.Skills.ShipGunner"),
-                "Small arms": game.i18n.localize("WARLOCK.Skills.SmallArms"),
-                "Thrown": game.i18n.localize("WARLOCK.Skills.Thrown"),
+                "Melee": game.i18n.localize("WARLOCK.Skills.Melee"),
+                "Gunnery": game.i18n.localize("WARLOCK.Skills.Gunnery"),
+                "Primitive Ranged": game.i18n.localize("WARLOCK.Skills.PrimitiveRanged"),
+                "Small Arms": game.i18n.localize("WARLOCK.Skills.SmallArms"),
             };
         }
 
